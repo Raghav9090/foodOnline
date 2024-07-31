360241fccee779c34be7fe5c464d9c645bad7e2d
Raghav9090 <raghavsinghal4@icloud.com>
2024-07-31 17:25:13 +0530

    email templates implementation

--- accounts/utils.py
+++ accounts/utils.py
@@ -18,19 +18,18 @@ def detectUser(user):
         redirectUrl='/admin'    
         return redirectUrl
     
-def send_verification_email(request, user,mail_subject,email_template):
-    from_email=settings.DEFAULT_FROM_EMAIL
-    current_site= get_current_site(request)   
-    print("Current site: ", current_site)
-    message=render_to_string(email_template,{
-        'user' : user,
-        'domain' : current_site,
-        'uid':urlsafe_base64_encode(force_bytes(user.pk)),
-        'token':default_token_generator.make_token(user)
+def send_verification_email(request, user, mail_subject, email_template):
+    from_email = settings.DEFAULT_FROM_EMAIL
+    current_site = get_current_site(request)
+    message = render_to_string(email_template, {
+        'user': user,
+        'domain': current_site,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': default_token_generator.make_token(user),
     })
     to_email = user.email
-    print("EMAIL SENIDNG ....")
-    mail = EmailMessage(mail_subject, message, from_email,to=[to_email])
+    mail = EmailMessage(mail_subject, message, from_email, to=[to_email])
+    mail.content_subtype = "html"
     mail.send()
 
 
